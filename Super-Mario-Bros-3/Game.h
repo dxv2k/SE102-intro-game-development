c808c1e1384e79517962b6f7d48bc91d4a43e7e9
knightV <9569076+dxv2k@users.noreply.github.com>
2020-10-02 00:58:03 +0700

    working on framework

---
+++ Super-Mario-Bros-3/Game.h
@@ -0,0 +1,23 @@
+#pragma once
+
+class Game {
+
+
+
+
+
+
+
+
+
+
+
+};
+
+
+
+
+
+
+
+
--- Super-Mario-Bros-3/main.cpp
+++ Super-Mario-Bros-3/main.cpp
@@ -1,9 +1,60 @@
+#include <windows.h>
+#include <d3d9.h>
+#include <d3dx9.h>
+
+#include <signal.h>
+#include <string.h>
+#include <stdio.h>
+#include <stdarg.h>
+#include <time.h>
+#include <stdlib.h>
+
+#define WINDOW_CLASS_NAME L"Super Mario Bros 3" 
+#define WINDOW_TITLE L"Super Mario Bros 3" 
+//#define WINDOW_ICON_PATH L""
+
+#define D3DCOLOR_WHITE D3DCOLOR_XRGB(255, 255, 255)
+#define BACKGROUND_COLOR D3DCOLOR_XRGB(0,0,0) 
 
 
+#define WINDOW_WIDTH 640 
+#define wiNDOW_HEIGHT 480
 
 
+HWND hWnd = 0; 
 
 
+LRESULT CALLBACK WinProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+	switch (message) {
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		break;
+	default:
+		return DefWindowProc(hWnd, message, wParam, lParam);
+	}
+	return 0;
+}
+
+void Update(DWORD dt) {
+
+}
+
+void Render() {
+
+}
+
+HWND CreateGameWindow(HINSTANCE hInstance, int nCmdShow, int ScreenWidth, int ScreenHeight) {
+
+}
+
+void Run() {
+
+}
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow) {
+
+}
 
 
 
