85d4e2afc1374d93772e31c2c55ced7d03729f5a
knightV <9569076+dxv2k@users.noreply.github.com>
2020-10-02 01:54:30 +0700

    trying to intialize empty window and basic of WINAPI first

--- Super-Mario-Bros-3/main.cpp
+++ Super-Mario-Bros-3/main.cpp
@@ -2,6 +2,8 @@
 #include <d3d9.h>
 #include <d3dx9.h>
 
+#include <Game.h> 
+
 #include <signal.h>
 #include <string.h>
 #include <stdio.h>
@@ -10,8 +12,9 @@
 #include <stdlib.h>
 
 #define WINDOW_CLASS_NAME L"Super Mario Bros 3" 
-#define WINDOW_TITLE L"Super Mario Bros 3" 
-//#define WINDOW_ICON_PATH L""
+//#define WINDOW_TITLE L"Super Mario Bros 3" 
+#define WINDOW_ICON_PATH L"mario.ico"
+#define MAIN_WINDOW_TITLE L'Super Mario Bros 3"
 
 #define D3DCOLOR_WHITE D3DCOLOR_XRGB(255, 255, 255)
 #define BACKGROUND_COLOR D3DCOLOR_XRGB(0,0,0) 
@@ -23,6 +26,7 @@
 
 HWND hWnd = 0; 
 
+Game* game; 
 
 LRESULT CALLBACK WinProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 {
@@ -36,15 +40,61 @@ LRESULT CALLBACK WinProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 	return 0;
 }
 
-void Update(DWORD dt) {
-
-}
+//void Update(DWORD dt) {
+//	Game::GetInstance()->Update(dt); 
+//}
 
 void Render() {
 
 }
 
 HWND CreateGameWindow(HINSTANCE hInstance, int nCmdShow, int ScreenWidth, int ScreenHeight) {
+	WNDCLASSEX wc;
+	wc.cbSize = sizeof(WNDCLASSEX);
+
+	wc.style = CS_HREDRAW | CS_VREDRAW;
+	wc.hInstance = hInstance;
+
+	wc.lpfnWndProc = (WNDPROC)WinProc;
+	wc.cbClsExtra = 0;
+	wc.cbWndExtra = 0;
+	wc.hIcon = (HICON)LoadImage(hInstance, WINDOW_ICON_PATH, IMAGE_ICON, 0, 0, LR_LOADFROMFILE);
+	wc.hCursor = LoadCursor(NULL, IDC_ARROW);
+	wc.hbrBackground = (HBRUSH)GetStockObject(WHITE_BRUSH);
+	wc.lpszMenuName = NULL;
+	wc.lpszClassName = WINDOW_CLASS_NAME;
+	wc.hIconSm = NULL;
+
+	RegisterClassEx(&wc);
+
+	HWND hWnd =
+		CreateWindow(
+			WINDOW_CLASS_NAME,
+			MAIN_WINDOW_TITLE,
+			WS_OVERLAPPEDWINDOW, // WS_EX_TOPMOST | WS_VISIBLE | WS_POPUP,
+			CW_USEDEFAULT,
+			CW_USEDEFAULT,
+			ScreenWidth,
+			ScreenHeight,
+			NULL,
+			NULL,
+			hInstance,
+			NULL);
+
+	if (!hWnd)
+	{
+		DWORD ErrCode = GetLastError();
+		DebugOut(L"[ERROR] CreateWindow failed! ErrCode: %d\nAt: %s %d \n", ErrCode, _W(__FILE__), __LINE__);
+		return 0;
+	}
+
+	ShowWindow(hWnd, nCmdShow);
+	UpdateWindow(hWnd);
+
+	SetDebugWindow(hWnd);
+
+	return hWnd;
+
 
 }
 
@@ -56,6 +106,8 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 
 }
 
+void LoadResources() {
 
+}
 
 
