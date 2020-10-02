ae0bf28eb5b4ade13b032c5231da558f776ef6b2
knightV <9569076+dxv2k@users.noreply.github.com>
2020-10-02 15:05:42 +0700

    intit WinAPI, WinProc and working on WinMain

--- Super-Mario-Bros-3/main.cpp
+++ Super-Mario-Bros-3/main.cpp
@@ -13,7 +13,7 @@
 #include <stdlib.h>
 
 #define WINDOW_CLASS_NAME L"Super Mario Bros 3" 
-//#define WINDOW_TITLE L"Super Mario Bros 3" 
+#define WINDOW_TITLE L"Super Mario Bros 3" 
 #define WINDOW_ICON_PATH L"mario.ico"
 #define MAIN_WINDOW_TITLE L'Super Mario Bros 3"
 
@@ -45,9 +45,28 @@ LRESULT CALLBACK WinProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 //	Game::GetInstance()->Update(dt); 
 //}
 
-void Render() {
-
-}
+//void Render() {
+//
+//	LPDIRECT3DDEVICE9 d3ddv = game->GetDirect3DDevice();
+//	LPDIRECT3DSURFACE9 bb = game->GetBackBuffer();
+//	LPD3DXSPRITE spriteHandler = game->GetSpriteHandler();
+//
+//	if (d3ddv->BeginScene())
+//	{
+//		// Clear back buffer with a color
+//		d3ddv->ColorFill(bb, NULL, BACKGROUND_COLOR);
+//
+//		spriteHandler->Begin(D3DXSPRITE_ALPHABLEND);
+//
+//		Game::GetInstance()->Render();
+//
+//		spriteHandler->End();
+//		d3ddv->EndScene();
+//	}
+//
+//	// Display back buffer content to the screen
+//	d3ddv->Present(NULL, NULL, NULL, NULL);
+//}
 
 HWND CreateGameWindow(HINSTANCE hInstance, int nCmdShow, int ScreenWidth, int ScreenHeight) {
 	WNDCLASSEX wc;
@@ -71,7 +90,7 @@ HWND CreateGameWindow(HINSTANCE hInstance, int nCmdShow, int ScreenWidth, int Sc
 	HWND hWnd =
 		CreateWindow(
 			WINDOW_CLASS_NAME,
-			MAIN_WINDOW_TITLE,
+			WINDOW_TITLE,
 			WS_OVERLAPPEDWINDOW, // WS_EX_TOPMOST | WS_VISIBLE | WS_POPUP,
 			CW_USEDEFAULT,
 			CW_USEDEFAULT,
@@ -82,17 +101,18 @@ HWND CreateGameWindow(HINSTANCE hInstance, int nCmdShow, int ScreenWidth, int Sc
 			hInstance,
 			NULL);
 
-	if (!hWnd)
-	{
-		DWORD ErrCode = GetLastError();
-		DebugOut(L"[ERROR] CreateWindow failed! ErrCode: %d\nAt: %s %d \n", ErrCode, _W(__FILE__), __LINE__);
-		return 0;
-	}
+
+	//if (!hWnd)
+	//{
+	//	DWORD ErrCode = GetLastError();
+	//	DebugOut(L"[ERROR] CreateWindow failed! ErrCode: %d\nAt: %s %d \n", ErrCode, _W(__FILE__), __LINE__);
+	//	return 0;
+	//}
 
 	ShowWindow(hWnd, nCmdShow);
 	UpdateWindow(hWnd);
 
-	SetDebugWindow(hWnd);
+	//SetDebugWindow(hWnd);
 
 	return hWnd;
 
