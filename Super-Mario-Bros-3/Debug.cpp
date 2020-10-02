958f9c62fe8835d82d3f97c8962057d3655817d8
knightV <9569076+dxv2k@users.noreply.github.com>
2020-10-03 00:46:37 +0700

    done with debug functionality

--- Super-Mario-Bros-3/Debug.cpp
+++ Super-Mario-Bros-3/Debug.cpp
@@ -1,45 +1,23 @@
 #include <Windows.h>
-#include "Debug.h"
+#include "debug.h"
 
-void DebugOut(wchar_t* fmt, ...)
-{
-	va_list argp;
-	va_start(argp, fmt);
-	wchar_t dbg_out[4096];
-	vswprintf_s(dbg_out, fmt, argp);
-	va_end(argp);
-	OutputDebugString(dbg_out);
-}
+HWND _hwnd = NULL;
 
-// DEBUG SUPPORT FUNCTIONS //////////////
-#define _W(x)  __W(x)
-#define __W(x)  L##x
-
-#define VA_PRINTS(s) {				\
-		va_list argp;				\
-		va_start(argp, fmt);		\
-		vswprintf_s(s, fmt, argp);	\
-		va_end(argp);				\
-}		
-
-void DebugOut(wchar_t* fmt, ...)
+void DebugOut(wchar_t *fmt, ...)
 {
 	wchar_t s[4096];
 	VA_PRINTS(s);
 	OutputDebugString(s);
 }
 
-void DebugOutTitle(wchar_t* fmt, ...)
+void DebugOutTitle(wchar_t *fmt, ...)
 {
 	wchar_t s[1024];
 	VA_PRINTS(s);
-	SetWindowText(hWnd, s);
+	SetWindowText(_hwnd, s);
 }
 
-
-
-
-
-
-
-
+void SetDebugWindow(HWND hwnd)
+{
+	_hwnd = hwnd;
+}
