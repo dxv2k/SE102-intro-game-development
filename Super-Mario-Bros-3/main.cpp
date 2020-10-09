319eab0455d0af00b50c017b667ba77ba116806f
KnightV <9569076+dxv2k@users.noreply.github.com>
2020-10-09 15:33:46 +0700

    clean up direct3D and fixed error in CreateGameWindow

--- Super-Mario-Bros-3/main.cpp
+++ Super-Mario-Bros-3/main.cpp
@@ -10,7 +10,7 @@
 #include <stdlib.h>
 
 #include <Game.h> 
-//#include <Debug.h>
+#include <Debug.h>
 
 #define WINDOW_CLASS_NAME L"Super Mario Bros 3" 
 #define WINDOW_TITLE L"Super Mario Bros 3" 
@@ -21,18 +21,8 @@
 #define BACKGROUND_COLOR D3DCOLOR_XRGB(0,0,0) 
 
 #define WINDOW_WIDTH 640 
-#define WINDOW_HEIGHT 480
-#define MAX_FRAME_RATE 120
-
-
-LPDIRECT3D9 d3d = NULL; 
-LPDIRECT3DDEVICE9 d3ddev = NULL; 
-LPDIRECT3DSURFACE9 backBuffer = NULL;
-
-int BackBufferWidth = 0;
-int BackBufferHeight = 0;
-
-LPD3DXSPRITE spriteHandler = NULL;			// Sprite helper library to help us draw 2D images 
+#define WINDOW_HEIGHT 480 
+#define MAX_FRAME_RATE 120 
 
 HWND hWnd = 0; 
 
@@ -54,27 +44,28 @@ void Update(DWORD dt) {
 	//Game::GetInstance()->Update(dt); 
 }
 
+// Render a frame
 void Render() {
 
 	LPDIRECT3DDEVICE9 d3ddv = game->GetDirect3DDevice();
 	LPDIRECT3DSURFACE9 bb = game->GetBackBuffer();
 	LPD3DXSPRITE spriteHandler = game->GetSpriteHandler();
 
-	//if (d3ddv->BeginScene())
-	//{
-	//	// Clear back buffer with a color
-	//	d3ddv->ColorFill(bb, NULL, BACKGROUND_COLOR);
+	if (d3ddv->BeginScene())
+	{
+		// Clear back buffer with a color
+		d3ddv->ColorFill(bb, NULL, BACKGROUND_COLOR);
 
-	//	spriteHandler->Begin(D3DXSPRITE_ALPHABLEND);
+		spriteHandler->Begin(D3DXSPRITE_ALPHABLEND);
 
-	//	Game::GetInstance()->Render();
+		//Game::GetInstance()->Render();
 
-	//	spriteHandler->End();
-	//	d3ddv->EndScene();
-	//}
+		spriteHandler->End();
+		d3ddv->EndScene();
+	}
 
-	//// Display back buffer content to the screen
-	//d3ddv->Present(NULL, NULL, NULL, NULL);
+	// Display back buffer content to the screen
+	d3ddv->Present(NULL, NULL, NULL, NULL);
 }
 
 HWND CreateGameWindow(HINSTANCE hInstance, int nCmdShow, int ScreenWidth, int ScreenHeight) {
@@ -111,17 +102,17 @@ HWND CreateGameWindow(HINSTANCE hInstance, int nCmdShow, int ScreenWidth, int Sc
 			NULL);
 
 
-	//if (!hWnd)
-	//{
-	//	DWORD ErrCode = GetLastError();
-	//	DebugOut(L"[ERROR] CreateWindow failed! ErrCode: %d\nAt: %s %d \n", ErrCode, _W(__FILE__), __LINE__);
-	//	return 0;
-	//}
+	if (!hWnd)
+	{
+		DWORD ErrCode = GetLastError();
+		OutputDebugString(L"[ERROR] CreateWindow failed!");
+		return 0;
+	}
 
 	ShowWindow(hWnd, nCmdShow);
 	UpdateWindow(hWnd);
 
-	//SetDebugWindow(hWnd);
+	SetDebugWindow(hWnd);
 	return hWnd;
 }
 
@@ -159,5 +150,4 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 void LoadResources() {
 
 }
-
 
