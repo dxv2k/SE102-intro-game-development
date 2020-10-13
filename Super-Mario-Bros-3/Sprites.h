83762fa2814134daf15f012b30cd243dd2b4b9da
knightV <9569076+dxv2k@users.noreply.github.com>
2020-10-13 16:59:06 +0700

    Update sprites, still a lot to work on

--- Super-Mario-Bros-3/Sprites.h
+++ Super-Mario-Bros-3/Sprites.h
@@ -33,7 +33,10 @@ class Sprites {
 	unordered_map<int, LPSPRITE> sprites; 
 
 public: 
+	void Add(int id, int left, int top, int right, int bottom, LPDIRECT3DTEXTURE9 tex);
+	LPSPRITE Get(int id);
 
+	static Sprites * GetInstance();
 
 };
 
