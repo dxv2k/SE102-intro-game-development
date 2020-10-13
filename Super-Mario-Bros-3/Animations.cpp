94aa3ec2079fb2487b8f709d6c200317333362f5
knightV <9569076+dxv2k@users.noreply.github.com>
2020-10-14 01:39:52 +0700

    working on Animations

--- Super-Mario-Bros-3/Animations.cpp
+++ Super-Mario-Bros-3/Animations.cpp
@@ -1,5 +1,11 @@
+#include "Animations.h"
+#include <Windows.h>
+#include <d3dx9.h>
+#include <unordered_map>
 
+void Animation::Add(int spriteID, DWORD time = 0) {
 
+}
 
 
 
--- Super-Mario-Bros-3/Animations.h
+++ Super-Mario-Bros-3/Animations.h
@@ -1,11 +1,49 @@
 #pragma once
+#include "Sprites.h"
+#include <Windows.h>
+#include <d3dx9.h>
+#include <unordered_map>
 
+/*
+	Sprite animation
+*/
+class AnimationFrame {
+	LPSPRITE sprite;
+	DWORD time;
 
+public:
+	AnimationFrame(LPSPRITE sprite, int time) { this->sprite = sprite; this->time = time; }
+	DWORD GetTime() { return time; }
+	LPSPRITE GetSprite() { return sprite; }
+};
 
+typedef AnimationFrame *LPANIMATION_FRAME;
 
+class Animation {
+	DWORD lastFrameTime;
+	int defaultTime;
+	int currentFrame;
+	vector<LPANIMATION_FRAME> frames;
+public:
+	Animation(int defaultTime) { this->defaultTime = defaultTime; lastFrameTime = -1; currentFrame = -1; }
 
+	void Add(int spriteId, 
+		DWORD time = 0 // Set time for how long this frame will freeze 
+	);
 
+	void Render(float x, float y);
+};
 
+typedef Animation *LPANIMATION;
 
+class Animations {
+	static Animations * __instance;
 
+	unordered_map<int, LPANIMATION> animations;
 
+public:
+	void Add(int id, LPANIMATION ani);
+	LPANIMATION Get(int id);
+
+	static Animations * GetInstance();
+};
