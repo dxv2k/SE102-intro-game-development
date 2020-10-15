8a5ce8553dc4508422d67880261e77beedf92ba9
KnightV <9569076+dxv2k@users.noreply.github.com>
2020-10-15 16:14:54 +0700

    add further implementation in the future

--- Super-Mario-Bros-3/KeyEventHandler.cpp
+++ Super-Mario-Bros-3/KeyEventHandler.cpp
@@ -1,9 +1,15 @@
+#include <Windows.h>
+#include <d3d9.h>
+#include <d3dx9.h>
 
+#include <KeyEventHandler.h>
 
+/*
+	In furture update, KeyEventHandler class will update with
+	default key control GameObject settings rather than manually 
+	inheritance each control setting for each GameObject 
 
-
-
-
+*/
 
 
 
