70a2197b84fb2f0453efdd43ef4015a333f4399d
knightV <9569076+dxv2k@users.noreply.github.com>
2020-10-15 15:40:52 +0700

    update key event handler

--- Super-Mario-Bros-3/KeyEventHandler.h
+++ Super-Mario-Bros-3/KeyEventHandler.h
@@ -1,5 +1,4 @@
 #pragma once
-
 #include <Windows.h>
 #include <d3d9.h>
 #include <d3dx9.h>
@@ -12,4 +11,9 @@ public:
 	virtual void OnKeyUp(int KeyCode) = 0; 
 };
 
+/*
+	TODO: Remember to create custom key so that we are able 
+	to switch to different status of the GameObject 
+	Ex: Mario -> 4 status -> Num1,2,3,4 -> status 1,2,3,4
+*/
 
