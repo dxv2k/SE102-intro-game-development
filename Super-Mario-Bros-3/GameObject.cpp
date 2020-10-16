869b7072394f34b76fb503cb6cb2a8237f88ec09
KnightV <9569076+dxv2k@users.noreply.github.com>
2020-10-16 14:41:47 +0700

    update GameObject, done core function for GameObject

--- Super-Mario-Bros-3/GameObject.cpp
+++ Super-Mario-Bros-3/GameObject.cpp
@@ -2,7 +2,7 @@
 #include "Game.h" 
 
 
-//vector <LPANIMATION> GameObject::animations;
+vector <LPANIMATION> GameObject::animations;
 
 GameObject::GameObject() {
 	x = y = 0; 
@@ -24,6 +24,10 @@ void GameObject::Render() {
 	// leave empty on purpose 
 }
 
+void GameObject::AddAnimation(int aniID) {
+	LPANIMATION ani = Animations::GetInstance()->Get(aniID); 
+	animations.push_back(ani); 
+}
 
 
 
