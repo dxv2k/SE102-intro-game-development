316d926b79bb91c4a2a1f696f31bdc45b460583d
knightV <9569076+dxv2k@users.noreply.github.com>
2020-10-19 03:56:17 +0700

    Update Render function for multi status of Mario

--- 03-Keyboard-States/Mario.cpp
+++ 03-Keyboard-States/Mario.cpp
@@ -18,16 +18,198 @@ void CMario::Update(DWORD dt)
 
 void CMario::Render()
 {
-	
-	int ani;
-	if (vx == 0)
+	//// Original Code	
+	//int ani;
+	//if (vx == 0)
+	//{
+	//	if (nx > 0) ani = MARIO_ANI_IDLE_RIGHT;
+	//	else ani = MARIO_ANI_IDLE_LEFT;
+	//}
+	//else if (vx > 0)
+	//	ani = MARIO_ANI_WALKING_RIGHT;
+	//else ani = MARIO_ANI_WALKING_LEFT;
+	int ani; 
+
+	if (status == MARIO_SMALL)
 	{
-		if (nx > 0) ani = MARIO_ANI_IDLE_RIGHT;
-		else ani = MARIO_ANI_IDLE_LEFT;
+		if (vx == 0)
+		{
+			if (nx > 0)
+			{
+				ani = MARIO_ANI_IDLE_RIGHT;
+			}
+			else
+			{
+				ani = MARIO_ANI_IDLE_LEFT;
+			}
+		}
+		else if (vx > 0)
+		{
+			ani = MARIO_ANI_WALKING_RIGHT;
+		}
+		else
+		{
+			ani = MARIO_ANI_WALKING_LEFT;
+		}
+
+		if (y < 100)
+		{
+			if (nx > 0)
+			{
+				ani = MARIO_ANI_JUMP_RIGHT;
+			}
+			else
+			{
+				ani = MARIO_ANI_JUMP_LEFT;
+			}
+		}
 	}
-	else if (vx > 0)
-		ani = MARIO_ANI_WALKING_RIGHT;
-	else ani = MARIO_ANI_WALKING_LEFT;
+
+	if (status == MARIO_BIG)
+	{
+		if (vx == 0)
+		{
+			if (nx > 0)
+			{
+				ani = MARIO_BIG_ANI_IDLE_RIGHT;
+			}
+			else
+			{
+				ani = MARIO_BIG_ANI_IDLE_LEFT;
+			}
+		}
+		else if (vx > 0)
+		{
+			ani = MARIO_BIG_ANI_WALKING_RIGHT;
+		}
+		else
+		{
+			ani = MARIO_BIG_ANI_WALKING_LEFT;
+		}
+
+		if (y < 100)
+		{
+			if (nx > 0)
+			{
+				ani = MARIO_BIG_ANI_JUMP_RIGHT;
+			}
+			else
+			{
+				ani = MARIO_BIG_ANI_JUMP_LEFT;
+			}
+		}
+
+		if (state == MARIO_STATE_SIT)
+		{
+			if (nx > 0)
+			{
+				ani = MARIO_BIG_ANI_SIT_RIGHT;
+			}
+			else
+			{
+				ani = MARIO_BIG_ANI_SIT_LEFT;
+			}
+		}
+	}
+
+	if (status == MARIO_RACOON)
+	{
+		if (vx == 0)
+		{
+			if (nx > 0)
+			{
+				ani = MARIO_RACOON_ANI_IDLE_RIGHT;
+			}
+			else
+			{
+				ani = MARIO_RACOON_ANI_IDLE_LEFT;
+			}
+		}
+		else if (vx > 0)
+		{
+			ani = MARIO_RACOON_ANI_WALKING_RIGHT;
+		}
+		else
+		{
+			ani = MARIO_RACOON_ANI_WALKING_LEFT;
+		}
+
+		if (y < 100)
+		{
+			if (nx > 0)
+			{
+				ani = MARIO_RACOON_ANI_JUMP_RIGHT;
+			}
+			else
+			{
+				ani = MARIO_RACOON_ANI_JUMP_LEFT;
+			}
+		}
+
+		if (state == MARIO_STATE_SIT)
+		{
+			if (nx > 0)
+			{
+				ani = MARIO_RACOON_ANI_SIT_RIGHT;
+			}
+			else
+			{
+				ani = MARIO_RACOON_ANI_SIT_LEFT;
+			}
+		}
+
+		if (state == MARIO_STATE_HIT_TAIL)
+		{
+			if (nx > 0)
+			{
+				ani = MARIO_RACOON_ANI_HIT_TAIL_RIGHT;
+			}
+			else
+			{
+				ani = MARIO_RACOON_ANI_HIT_TAIL_LEFT;
+			}
+		}
+	}
+
+	if (status == MARIO_FIRE)
+	{
+		if (vx == 0)
+		{
+			if (nx > 0)
+				ani = MARIO_FIRE_ANI_IDLE_RIGHT;
+			else
+				ani = MARIO_FIRE_ANI_IDLE_LEFT;
+		}
+		else if (vx > 0)
+			ani = MARIO_FIRE_ANI_WALKING_RIGHT;
+		else
+			ani = MARIO_FIRE_ANI_WALKING_LEFT;
+
+		if (y < 100)
+		{
+			if (nx > 0)
+				ani = MARIO_FIRE_ANI_JUMP_RIGHT;
+			else
+				ani = MARIO_FIRE_ANI_JUMP_LEFT;
+		}
+
+		if (state == MARIO_STATE_SIT)
+		{
+			if (nx > 0)
+				ani = MARIO_FIRE_ANI_SIT_RIGHT;
+			else
+				ani = MARIO_FIRE_ANI_SIT_LEFT;
+		}
+
+		if (state == MARIO_STATE_SHOOT_FIRE)
+		{
+			if (nx > 0)
+				ani = MARIO_FIRE_ANI_SHOOT_RIGHT;
+			else
+				ani = MARIO_FIRE_ANI_SHOOT_LEFT;
+		}
+	}
+
 
 	animations[ani]->Render(x, y);
 	
@@ -63,20 +245,11 @@ void CMario::SetState(int state)
 	}
 }
 
-void CMario::SetStatus(int status) {
-	CGameObject::SetStatus(status); 
-	switch (status)
-	{
-	case SMALL_MARIO: 
-		status = 10;
-		break; 
-	case SUPER_MARIO: 
-		status = 11;
-		break; 
-	}
-
+CMario::CMario() {
+	//status = MARIO_SMALL; 
+	//status = MARIO_BIG; 
+	//status = MARIO_RACOON; 
+	//status = MARIO_FIRE; 
+	status = MARIO_STAR; 
 }
-
-
-
 
