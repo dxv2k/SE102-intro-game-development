fae032fea066a2868a7f1f8eab4013ba33ff4023
knightV <9569076+dxv2k@users.noreply.github.com>
2020-10-20 01:32:53 +0700

    working on create animations list for Mario with TiledMap

---
+++ Mario-Animations-TiledMap/Mario_Animation.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.2" name="Mario_Animations" tilewidth="32" tileheight="32" tilecount="637" columns="13">
+ <image source="../Super-Mario-Bros-3/textures/mario.png" trans="ff00ff" width="446" height="1598"/>
+</tileset>
