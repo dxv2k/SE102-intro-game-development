0701efffbe81033d336a74c5dd46449633a93c66
knightV <9569076+dxv2k@users.noreply.github.com>
2020-10-22 12:24:34 +0700

    trying to use tiledmap, nothing serious here

---
+++ Mario-Animations-TiledMap/small_mario.tsx
@@ -0,0 +1,10 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.2" name="small_mario" tilewidth="18" tileheight="18" tilecount="18" columns="18">
+ <image source="small_mario.png" trans="29587c" width="324" height="18"/>
+ <tile id="0">
+  <animation>
+   <frame tileid="0" duration="100"/>
+   <frame tileid="1" duration="100"/>
+  </animation>
+ </tile>
+</tileset>
