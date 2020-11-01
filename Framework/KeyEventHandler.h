2875ec373c0270b85b571ced5930325b58deb946
KnightV <9569076+dxv2k@users.noreply.github.com>
2020-11-01 17:51:20 +0700

    compelte create abstract class for KeyEventHandler

--- Framework/KeyEventHandler.h
+++ Framework/KeyEventHandler.h
@@ -1,1 +1,12 @@
 #pragma once
+#include <Windows.h>
+
+class KeyEventHandler {
+public: 
+	virtual void KeyState(BYTE *state); 
+	virtual void OnKeyUp(int KeyCode);
+	virtual void OnKeyDown(int KeyCode);
+};
+
+typedef KeyEventHandler* LPKEYEVENTHANDLER; 
+
