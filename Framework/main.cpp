5838164d8be890dcd4094eb253e7db9b92bdd6e3
KnightV <9569076+dxv2k@users.noreply.github.com>
2020-11-01 15:56:34 +0700

    intial files to rebuilt Framework

---
+++ Framework/main.cpp
@@ -0,0 +1,36 @@
+#include <windows.h>
+#include <d3d9.h>
+#include <d3dx9.h>
+
+//#include "Utils.h"
+#include "Game.h"
+#include "GameObject.h"
+#include "Textures.h"
+
+//#include "Mario.h"
+//#include "Brick.h"
+//#include "Goomba.h"
+//
+//#include "PlayScence.h"
+
+#define WINDOW_CLASS_NAME L"SampleWindow"
+#define MAIN_WINDOW_TITLE L"SAMPLE 05 - SCENCE MANAGER"
+
+#define BACKGROUND_COLOR D3DCOLOR_XRGB(255, 255, 200)
+#define SCREEN_WIDTH 320
+#define SCREEN_HEIGHT 240
+
+#define MAX_FRAME_RATE 120
+
+Game *game;
+
+
+
+
+
+
+
+
+
+
+
