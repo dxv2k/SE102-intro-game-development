c923623de5571ef06d82d526a0475b743084890f
KnightV <9569076+dxv2k@users.noreply.github.com>
2020-11-05 21:53:04 +0700

    complete Singleton Manager

--- Framework2/SingletonManager.cpp
+++ Framework2/SingletonManager.cpp
@@ -1,9 +1,15 @@
 #include "SingletonManager.h"
 
+void SingletonManager::AddSingleton(Singleton *singleton) {
+	list_singleton.push_back(singleton); 
+}
 
-
-
-
+void SingletonManager::clearSingletonList() {
+	for (auto s : list_singleton) {
+		delete s; 
+	}
+	list_singleton.clear(); 
+}
 
 
 
--- Framework2/SingletonManager.h
+++ Framework2/SingletonManager.h
@@ -5,13 +5,22 @@
 using namespace std; 
 class SingletonManager {
 private: 
-	vector <Singleton*> singletons; 
+	vector <Singleton*> list_singleton; 
 public: 
-
+	template <typename S> S* GetSingleton();  
+	void AddSingleton(Singleton *singleton); 
+	void clearSingletonList(); 
 
 };
 
-
+template <typename S> 
+inline S* SingletonManager::GetSingleton() {
+	for (auto singleton : list_singleton) {
+		S* currentSingleton = dynamic_cast<S*><singleton>
+		if (currentSingleton) return currentSingleton; 
+	}
+	return nullptr; 
+}
 
 
 
