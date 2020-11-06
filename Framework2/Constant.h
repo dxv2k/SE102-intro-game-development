0d5ec27b82e2a35141858013415058e883788154
KnightV <9569076+dxv2k@users.noreply.github.com>
2020-11-07 01:33:44 +0700

    Update game database

--- Framework2/Constant.h
+++ Framework2/Constant.h
@@ -1,10 +1,11 @@
-#define MARIO_TEXTURE "mario_texture"
-#define BBOX_TEXTURE "bbox_texture"
-#define ENEMY_TEXTURE "enenmy_texture"
-#define FIREBALL_TEXTURE "fireball_texture"
+#define MARIO_TEXTURE "tex-mario"
+#define BBOX_TEXTURE "tex-bbox"
+#define ENEMY_TEXTURE "tex-enemy"
+#define FIREBALL_TEXTURE "tex-fire-ball"
+
 #define INTRO_TEXTURE "intro_texture"
-#define MISC_TEXTURE "misc_texture"
+#define MISC_TEXTURE "tex-misc"
 #define UI_TEXTURE "UI_texture"
 
-
+#define TRANSPARENT_COLOR D3DCOLOR_XRGB(255, 255, 255)
 
