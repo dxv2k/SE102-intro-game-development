05143f785b6c8d1322b2e8bc3574afee3ba8667f
KnightV <9569076+dxv2k@users.noreply.github.com>
2020-11-07 20:09:07 +0700

    Add rectangle with float dtype

---
+++ Framework2/RectFloat.h
@@ -0,0 +1,15 @@
+// Why does this exists? 
+// -> Because Rect in Windows.h is long dtype 
+// which is not suitable for updating world and GameObj
+struct RectFloat {
+	float left, top, right, bottom; 
+};
+
+RECT r; 
+
+
+
+
+
+
+
