1a99aa70236cd2e3ba4581951acf6ad0031f5590
knightV <9569076+dxv2k@users.noreply.github.com>
2020-11-11 02:40:14 +0700

    More comment, add local position (relative position) to Transform.h

--- Framework2/Transform.h
+++ Framework2/Transform.h
@@ -6,34 +6,30 @@
 
 class Transform {
 	/*
-	Position, scale and rotation of an GameObject
-	It's used to store and manipulate the positino, scale and rotation of every GameObject
+	Position, local position, scale and rotation of an GameObject
+	It's used to store and manipulate the position, scale and rotation of every GameObject
+	and inhertied object of Transform class
 	*/
 
-	//D3DXVECTOR2 position; //World position 
-	//D3DXVECTOR2 localPosition; 
-
-	//D3DXVECTOR2 scale;
-	//float rotationAngle; // Receive with Degree but when Draw must convert into Radian 
-
 public:
-	
 	D3DXVECTOR2 position; //World position 
+	
+	// localPosition: or relative position to the parent transform 
 	D3DXVECTOR2 localPosition; 
 
 	D3DXVECTOR2 scale;
 	float rotationAngle; // Receive with Degree but when Draw must convert into Radian 
 
 	Transform(
-		// Did not test the default parameter yet
 		D3DXVECTOR2 position = D3DXVECTOR2(0.0f,0.0f),
+		D3DXVECTOR2 localPosition = D3DXVECTOR2(0.0f,0.0f),
 		D3DXVECTOR2 scale = D3DXVECTOR2(1.0f,1.0f),
 		float rotationAngle = 0.0f
 	);
 	~Transform();
 
 	// Translate: translate the object by multiply current position (world position) 
-	// to distance. Useful for Camera 
+	// to distance. Useful for Camera, update moving object with FixedUpdate() 
 	void Translate(D3DXVECTOR2 newCoordinate);
 
 	void SetPosition(D3DXVECTOR2 newPos) { this->position = newPos; }
