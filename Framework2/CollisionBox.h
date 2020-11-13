30f5d4d80ba2f6aa228a4a1a165a0b1cc3c96873
knightV <9569076+dxv2k@users.noreply.github.com>
2020-11-14 03:14:17 +0700

    add RigidBody, GameObj, typedef for CollisionBox

--- Framework2/CollisionBox.h
+++ Framework2/CollisionBox.h
@@ -5,37 +5,32 @@
 #include <d3d9.h>
 
 #include "Utils.h"
-#include "Sprites.h"
 #include "GameObject.h"
-#include "AnimationFrame.h"
 #include "Transform.h"
-
+#include "RigidBody.h"
 #include "MonoBehaviour.h"
 
-//class CollisionBox : protected MonoBehaviour
-//{
-//protected: 
-//	Transform transform; 
-//
-//public: 
-//	CollisionBox();
-//	~CollisionBox();
-//
-//};
 
 class CollisionBox : protected MonoBehaviour,
-					public Transform
-{
+					public Transform {
 protected: 
 	D3DXVECTOR2 sizeBox; //height and width respectively 
+	//D3DXVECTOR2 center; // center of the Collision Box  by default based on sizeBox 
 	LPGAMEOBJECT gameObj; 
-
+	LPRIGIDBODY rigidBody; 
 
 public: 
 	CollisionBox();
 	~CollisionBox();
-	LPGAMEOBJECT getGameObject() { return this->gameObj; }
 
+	void SetSizeBox(D3DXVECTOR2 size) { this->sizeBox = size; }
+	D3DXVECTOR2 GetSizeBox() { return this->sizeBox; }
+
+	void attachRigidBody(LPRIGIDBODY& rb) { this->rigidBody = rb;  }
+	LPRIGIDBODY getRigidBody() { return this->rigidBody; }
+
+	void attachGameObject(LPGAMEOBJECT& obj) { this->gameObj = obj; }
+	LPGAMEOBJECT getGameObject() { return this->gameObj; }
 };
 
-
+typedef CollisionBox* LPCOLLISIONBOX; 
