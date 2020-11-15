600df758b73a6e7858a3b48bd9dd4fae73aca194
knightV <9569076+dxv2k@users.noreply.github.com>
2020-11-15 12:54:07 +0700

    no idea why this fix the error...really no idea

--- Framework2/Collision.cpp
+++ Framework2/Collision.cpp
@@ -1,14 +1,14 @@
-#include "Collision.h"
+//#include "Collision.h"
 
-Collision2D::Collision2D() {
-	this->t = 0; 
-	this->nx = 0; 
-	this->ny = 0; 
-	this->dx = 0; 
-	this->dy = 0; 
-}
-
-Collision2D::~Collision2D() {
-	// Empty on purpose 
-}
+//Collision2D::Collision2D() {
+//	this->t = 0; 
+//	this->nx = 0; 
+//	this->ny = 0; 
+//	this->dx = 0; 
+//	this->dy = 0; 
+//}
+//
+//Collision2D::~Collision2D() {
+//	// Empty on purpose 
+//}
 
--- Framework2/Collision.h
+++ Framework2/Collision.h
@@ -3,24 +3,54 @@
 #include "MonoBehaviour.h"
 #include "CollisionBox.h"
 
-using namespace std; 
-
-class Collision2D: protected MonoBehaviour {
-public: 
+//Testing purpose 
+struct CollisionEvent
+{
+	LPCOLLIDERBOX objCollider;
 	float t, nx, ny;
-	float  dx, dy;
+	float dx, dy;		// *RELATIVE* movement distance between this object and obj
 
-	Collision2D(); 
-	~Collision2D(); 
-
-	/*
-		TODO: 
-		-	Add SweptAABB
-		-	Filter Collision 
-		-	Calculate  Potential Collision 
-	*/
-	bool Comparator(const Collision2D &otherCollision) {
-		return this->t < otherCollision.t; 
+	CollisionEvent(float t, 
+		float nx, 
+		float ny, 
+		float dx = 0, 
+		float dy = 0, 
+		LPCOLLIDERBOX obj = NULL)
+	{
+		this->t = t;
+		this->nx = nx;
+		this->ny = ny;
+		this->dx = dx;
+		this->dy = dy;
+		this->objCollider = obj;
 	}
+	
+	static bool compare(const LPCOLLISIONEVENT& thisEvent, 
+						LPCOLLISIONEVENT& otherEvent) {
+		return (thisEvent->t < otherEvent->t) ? 1 : 0;
+	}
+
 };
-typedef Collision2D* LPCOLLISIONEVENT
+typedef CollisionEvent* LPCOLLISIONEVENT
+
+
+
+
+//using namespace std; 
+//
+//class Collision2D: protected MonoBehaviour {
+//public: 
+//	float t, nx, ny;
+//	float  dx, dy;
+//
+//	Collision2D(); 
+//	~Collision2D(); 
+//
+//	/*
+//		TODO:Add SweptAABB & Filter Collision & Calculate Potential Collision 
+//	*/
+//	bool Comparator(const Collision2D &otherCollision) {
+//		return this->t < otherCollision.t; 
+//	}
+//};
+//typedef Collision2D* LPCOLLISIONEVENT
